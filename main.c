3f4234d090fac42e7d63c8215fd6f95273b63127
mgeltz <icy.yew02@gmail.com>
2025-11-03 16:25:57 -0600

    starting on UI, validation.h

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "validation.h"
 
 int main() {
-	
+	printf("Select MODE: \n1. SIMULATION\n2. IMPORT NEW CASE\n3. STATISTICAL ANALYSIS ON LOG FILE\n");
+	printf("Input the number of your selection: ");
+	int modeSelect = 0;
+	do {
+		if (scanf("%d", &modeSelect) != 1) {
+			//non-int input, clean stdin
+			int c;
+			while ((c = getchar()) != '\n' && c != EOF) {}
+			modeSelect = 0;
+		}
+	}while (!validateInt(modeSelect));
 }
